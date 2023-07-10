33ac68164ca9a64136f74dc3b585a6eae2697045
Артём <pokinko1999@gmail.com>
2023-07-11 01:33:01 +0300

    исправлен pom

--- ewm-stats-service/dto/src/main/java/ru/practicum/HitDto.java
+++ ewm-stats-service/dto/src/main/java/ru/practicum/HitDto.java
@@ -5,18 +5,13 @@ import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
-import javax.validation.constraints.NotBlank;
-
 @Value
 @Builder
 @Jacksonized
 public class HitDto {
     Long id;
-    @NotBlank
     String app;
-    @NotBlank
     String uri;
-    @NotBlank
     String ip;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     String timestamp;
--- ewm-stats-service/dto/src/main/java/ru/practicum/StatsDto.java
+++ ewm-stats-service/dto/src/main/java/ru/practicum/StatsDto.java
@@ -4,15 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
-import javax.validation.constraints.NotBlank;
-
 @Value
 @Builder
 @AllArgsConstructor
 public class StatsDto {
-    @NotBlank
     String app;
-    @NotBlank
     String uri;
     Long hits;
 }
