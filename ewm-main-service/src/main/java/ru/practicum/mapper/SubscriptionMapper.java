3d2e82ed4333a0ec49ecd1d62f363ae3647da7da
Артём <pokinko1999@gmail.com>
2023-07-22 18:27:36 +0300

    косметические изменения

--- ewm-main-service/src/main/java/ru/practicum/mapper/SubscriptionMapper.java
+++ ewm-main-service/src/main/java/ru/practicum/mapper/SubscriptionMapper.java
@@ -7,9 +7,9 @@ import ru.practicum.model.subscription.Subscription;
 @UtilityClass
 public class SubscriptionMapper {
 
-    public SubscriptionDto toSubscriptionDto(Subscription subscription){
-        return SubscriptionDto.builder().
-                id(subscription.getId())
+    public SubscriptionDto toSubscriptionDto(Subscription subscription) {
+        return SubscriptionDto.builder()
+                .id(subscription.getId())
                 .subscriber(UserMapper.toUserDto(subscription.getSubscriber()))
                 .creator(UserMapper.toUserDto(subscription.getCreator()))
                 .build();
